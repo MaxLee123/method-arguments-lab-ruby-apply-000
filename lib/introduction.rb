8c7332da61842bbbf5f1d0fc07459484b13f799c
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-19 10:36:53 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+introduction("Josh")
+
+def introduction_with_language(name2, language)
+  puts "Hi, my name is #{name2} and I am learning to program #{language}.\n"
+end
+introduction_with_language("Dan", Ember.js)
