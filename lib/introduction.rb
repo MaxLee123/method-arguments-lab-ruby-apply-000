0de0756ef84cabdb23ce42851433ed6d25d84ae9
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-19 10:35:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,4 +8,4 @@ def introdcution_with_language(name2, language)
   puts "Hi, my name is #{name2} and I am learning to program #{language}.\n"
 end
 
-introdcution_with_language("Dan", "Ember.js")
+introdcution_with_language("Dan", Ember.js)
